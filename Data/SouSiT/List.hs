860b7ccc8985ed6e2a6951296afae2d28945b816
Mario Siegenthaler <msiegenthaler@gmx.net>
2012-08-26 20:20:16 +0200

    fix List to the new Sink

--- Data/SouSiT/List.hs
+++ Data/SouSiT/List.hs
@@ -9,11 +9,11 @@ import Data.SouSiT
 
 -- | A source containing the elements of the list
 listSource :: Monad m => [a] -> BasicSource2 m a
-listSource l = BasicSource2 $ feedSinkList l
+listSource l = BasicSource2 $ feedList l
 
 
 -- | A sink that collects all input into a list. Does never say SinkDone.
 listSink :: Monad m => Sink a m [a]
-listSink = listSink' []
-listSink' xs = SinkCont next (return xs)
-	where next x = return $ listSink' (xs ++ [x])
+listSink = step []
+    where step xs = Sink $ return $ Cont add (return xs)
+            where add x = return $ step (xs ++ [x])
