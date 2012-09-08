e116039aaa7d1bd23b4c4a2dbb6b21b2e0819cb6
Mario Siegenthaler <msiegenthaler@gmx.net>
2012-09-08 22:57:13 +0200

    generalize type signatures to (Sink -> Sink).

--- Data/SouSiT/Transform.hs
+++ Data/SouSiT/Transform.hs
@@ -16,29 +16,30 @@ import Data.SouSiT.Source
 type Transform a b = forall m r . Monad m => Sink b m r -> Sink a m r
 
 -- | apply a transform to a sink
-transformSink :: Monad m => Transform a b -> Sink b m r -> Sink a m r
+transformSink :: Monad m => (Sink b m r -> Sink a m r) -> Sink b m r -> Sink a m r
 transformSink t = t
 
 -- | apply a transform to a sink
-(=$) :: Monad m => Transform a b -> Sink b m r -> Sink a m r
-(=$) = transformSink
+(=$) :: Monad m => (Sink b m r -> Sink a m r) -> Sink b m r -> Sink a m r
+(=$) t = t
 infixl 2 =$
 
 
 -- | apply a transform to a source
-transformSource :: (Source src, Monad m) => Transform a b -> src m a -> BasicSource m b
+--transformSource :: (Source src, Monad m) => Transform a b -> src m a -> BasicSource m b
+transformSource :: (Source src, Monad m) => (forall r . Sink b m r -> Sink a m r) -> src m a -> BasicSource m b
 transformSource t src = BasicSource $ transfer src . transformSink t
 
 -- | apply a transform to a source
-($=) :: (Source src, Monad m) => src m a -> Transform a b -> BasicSource m b
+($=) :: (Source src, Monad m) => src m a -> (forall r . Sink b m r -> Sink a m r) -> BasicSource m b
 ($=) = flip transformSource
 infixl 1 $=
 
 
 -- | merges two transforms into one
-mergeTransform :: Transform a b -> Transform b c -> Transform a c
+mergeTransform :: Monad m => (Sink b m r -> Sink a m r) -> (Sink c m r -> Sink b m r) -> (Sink c m r -> Sink a m r)
 mergeTransform t1 t2 = t1 . t2
 
 -- | merges two transforms into one
-(=$=) :: Transform a b -> Transform b c -> Transform a c
+(=$=) :: Monad m => (Sink b m r -> Sink a m r) -> (Sink c m r -> Sink b m r) -> (Sink c m r -> Sink a m r)
 (=$=) = mergeTransform
