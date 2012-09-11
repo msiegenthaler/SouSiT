6eab4624faee2a41137c6a3443c1129a7aa7b18f
Mario Siegenthaler <msiegenthaler@gmx.net>
2012-09-11 21:03:50 +0200

    readd countTrans to PerformanceTest.

--- test/PerformanceTest.hs
+++ test/PerformanceTest.hs
@@ -1,18 +1,19 @@
+{-# LANGUAGE BangPatterns #-}
 module Main (
     main
 ) where
 
 import Criterion.Main
-import Data.SouSiT.Source
+import Data.SouSiT
 import Data.SouSiT.Sink
--- import qualified Data.SouSiT.Trans as T
+import qualified Data.SouSiT.Trans as T
 import Data.SouSiT.List
 import Control.Monad.Identity
 
 
 countSink :: (Num n, Monad m) => Sink a m n
 countSink = step 0
-  where step n = contSink f (return n)
+  where step !n = contSink f (return n)
           where f _ = step (n + 1)
 
 firstSink :: Monad m => Sink a m a
@@ -26,15 +27,15 @@ countList n = runIdentity (elemCountSource n $$ countSink)
 countListIO :: Int -> IO Int
 countListIO n = elemCountSource n $$ countSink
 
---countListTrans n = runIdentity (elemCountSource n $$ T.count =$ firstSink)
+countListTrans n = runIdentity (elemCountSource n $$ T.count =$ firstSink)
 
---countListTransIO :: Int -> IO Int
---countListTransIO n = elemCountSource n $$ T.count =$ firstSink
+countListTransIO :: Int -> IO Int
+countListTransIO n = elemCountSource n $$ T.count =$ firstSink
 
 
 main = defaultMain [
             bench "count elems from listSource in Identity" $ whnf countList c,
-            bench "count elems from listSource in IO" $ countListIO c
-            --bench "count elems in Trans from listSource in Identity" $ whnf countListTrans c,
-            --bench "count elems in Trans from listSource in IO" $ countListTransIO c
+            bench "count elems from listSource in IO" $ countListIO c,
+            bench "count elems in Trans from listSource in Identity" $ whnf countListTrans c,
+            bench "count elems in Trans from listSource in IO" $ countListTransIO c
     ] where c = 100000
