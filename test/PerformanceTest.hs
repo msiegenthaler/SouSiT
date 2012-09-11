cf5fb1c9fca8633762792ccae5057c31a3ce944d
Mario Siegenthaler <msiegenthaler@gmx.net>
2012-09-11 22:08:33 +0200

    add more performance tests (for take and drop).

--- test/PerformanceTest.hs
+++ test/PerformanceTest.hs
@@ -32,10 +32,46 @@ countList n = elemCountSource n $$ countSink
 countListTrans :: Monad m => Int -> m Int
 countListTrans n = elemCountSource n $$ T.count =$ firstSink
 
+dropListFew :: Monad m => Int -> m Int
+dropListFew n = elemCountSource n $$ T.drop 100 =$ countSink
+
+dropListMany :: Monad m => Int -> m Int
+dropListMany n = elemCountSource n $$ T.drop (n-100) =$ countSink
+
+dropUntilListFew :: Monad m => Int -> m Int
+dropUntilListFew n = elemCountSource n $$ T.dropUntil (>100) =$ countSink
+
+dropUntilListMany :: Monad m => Int -> m Int
+dropUntilListMany n = elemCountSource n $$ T.dropUntil (>(n-100)) =$ countSink
+
+takeListFew :: Monad m => Int -> m Int
+takeListFew n = elemCountSource n $$ T.take 100 =$ countSink
+
+takeListMany :: Monad m => Int -> m Int
+takeListMany n = elemCountSource n $$ T.take (n-100) =$ countSink
+
+takeUntilListFew :: Monad m => Int -> m Int
+takeUntilListFew n = elemCountSource n $$ T.takeUntil (>100) =$ countSink
+
+takeUntilListMany :: Monad m => Int -> m Int
+takeUntilListMany n = elemCountSource n $$ T.takeUntil (>(n-100)) =$ countSink
+
 
 main = defaultMain [
-            bench "count elems from listSource in Identity" $ whnf (pure . countList) c,
-            bench "count elems from listSource in IO" $ io $ countList $ c,
-            bench "count elems in Trans from listSource in Identity" $ whnf (pure . countListTrans) c,
-            bench "count elems in Trans from listSource in IO" $ io $ countListTrans $ c
+            bgroup "Monad comparision" [
+                bench "count elems from listSource in Identity" $ whnf (pure . countList) c,
+                bench "count elems from listSource in IO" $ io $ countList c,
+                bench "count elems in Trans from listSource in Identity" $ whnf (pure . countListTrans) c,
+                bench "count elems in Trans from listSource in IO" $ io $ countListTrans c
+            ],
+            bgroup "Transforms" [
+                bench "drop few elems" $ io $ dropListFew c,
+                bench "drop many elems" $ io $ dropListMany c,
+                bench "dropUntil few elems" $ io $ dropUntilListFew c,
+                bench "dropUntil many elems" $ io $ dropUntilListMany c,
+                bench "take few elems" $ io $ takeListFew c,
+                bench "take many elems" $ io $ takeListMany c,
+                bench "takeUntil few elems" $ io $ takeUntilListFew c,
+                bench "takeUntil many elems" $ io $ takeUntilListMany c
+            ]
     ] where c = 100000
